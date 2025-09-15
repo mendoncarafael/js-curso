bc44a678b8b892c19e85a4a670714bc7739b0eb1
mendoncarafael <mendocarafael23@gmail.com>
2025-09-15 16:04:16 -0300

    section D complete, and start E

--- exec2/script.js
+++ exec2/script.js
@@ -1,24 +1,66 @@
-var body = document.getElementById('body');
-var time = document.getElementById('hours');
-var image = document.querySelector('img#image');
+var verify = document.getElementById('checkButton')
+var image = document.getElementById('image')
 
-var date = new Date();
-time = date.getHours();
+verify.addEventListener('click', () => {
+  var contImg = document.getElementById('imgCont')
+  var anoNas = document.getElementById('yearInput').value
+  var date = new Date()
+  var idade = date.getFullYear() - Number(anoNas)
+  var sexF = document.getElementById('sexF') // input
+  var sexM = document.getElementById('sexM')
+  var result = document.getElementById('result')
+  // contImg.classList.toggle('expandido');
+  contImg.style.height = '420px'
+  image.style.display = 'block'
 
-if (time < 12 && time >= 6) {
-  image.src = 'image/dia.jpg';
-  hours.innerHTML = `Agora são ${time} horas.`;
-  body.style.background = '#e2cd9f';
-} else if (time >= 12 && time <= 18) {
-  image.src = 'image/tarde.jpg';
-  hours.innerHTML = `Agora são ${time} horas da tarde.`;
-  body.style.background = '#b9846f';
-} else if (time > 18 && time < 24) {
-  image.src = 'image/noite.jpg';
-  hours.innerHTML = `Agora são ${time} horas da noite.`;
-  body.style.background = '#515154';
-} else {
-  image.src = 'image/noite.jpg';
-  hours.innerHTML = `Agora são ${time} horas da madrugada.`;
-  body.style.background = '#1a1a1aff';
-}
+  if (sexF.checked) {
+    if (idade >= 5 && idade < 10) {
+      image.src = 'image/criancaF.jpeg'
+      result.innerHTML = `Sexo: ${sexF.value} «» Idade: ${idade}`
+      result.innerHTML += `<br><strong>Criança<strong>`
+    } else if (idade >= 10 && idade < 18) {
+      result.innerHTML = `Sexo: ${sexF.value} «» Idade: ${idade}`
+      result.innerHTML += `<br><strong>Jovem<strong>`
+      image.src = 'image/jovemF.jpeg'
+    } else if (idade >= 18 && idade < 65) {
+      image.src = 'image/adultaF.jpeg'
+      result.innerHTML = `Sexo: ${sexF.value} «» Idade: ${idade}`
+      result.innerHTML += `<br><strong>Adulta<strong>`
+    } else if (idade >= 65 && idade < 120) {
+      image.src = 'image/idosaF.jpg'
+      result.innerHTML = `Sexo: ${sexF.value} «» Idade: ${idade}`
+      result.innerHTML += `<br><strong>Idosa<strong>`
+    } else if (idade >= 0 && idade < 5) {
+      image.src = 'image/bebeF.jpeg'
+      result.innerHTML = `Sexo: ${sexF.value} «» Idade: ${idade}`
+      result.innerHTML += `<br><strong>Nénem<strong>`
+    } else {
+      window.alert('Error')
+    }
+  } else if (sexM.checked) {
+    if (idade >= 5 && idade < 10) {
+      image.src = 'image/criancaM.jpeg'
+      result.innerHTML = `Sexo: ${sexF.value} «» Idade: ${idade}`
+      result.innerHTML += `<br><strong>Criança<strong>`
+    } else if (idade >= 10 && idade < 18) {
+      result.innerHTML = `Sexo: ${sexF.value} «» Idade: ${idade}`
+      result.innerHTML += `<br><strong>Jovem<strong>`
+      image.src = 'image/jovemM.jpeg'
+    } else if (idade >= 18 && idade < 65) {
+      result.innerHTML = `Sexo: ${sexF.value} «» Idade: ${idade}`
+      result.innerHTML += `<br><strong>Adulto<strong>`
+      image.src = 'image/adultoM.jpeg'
+    } else if (idade >= 65 && idade < 120) {
+      result.innerHTML = `Sexo: ${sexF.value} «» Idade: ${idade}`
+      result.innerHTML += `<br><strong>Idoso<strong>`
+      image.src = 'image/idosoM.jpg'
+    } else if (idade >= 0 && idade < 5) {
+      result.innerHTML = `Sexo: ${sexF.value} «» Idade: ${idade}`
+      image.src = 'image/bebeM.jpeg'
+    } else {
+      window.alert('Error')
+    }
+  }
+
+
+})
---
+++ exec3/script.js
@@ -0,0 +1,14 @@
+let btn = document.getElementById('button')
+
+btn.addEventListener('click', () => {
+let inicio = document.getElementById('inicio').value
+let fim = document.getElementById('fim').value
+let passo = document.getElementById('passo').value
+
+    for (inicio; inicio <= fim; passo++) {
+    console.log(inicio)
+}                                                                                                                   
+})
+
+
+                        
